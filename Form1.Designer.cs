812832b63c4917127a3f03bd94a1550335c9564d
zhangbo7364 <zhangbo7364@163.com>
2014-03-06 13:17:52 +0800

    第一次提交

---
+++ Form1.Designer.cs
@@ -0,0 +1,141 @@
+﻿namespace vpms
+{
+    partial class FrmMain
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSend = new System.Windows.Forms.Button();
+            this.txtController = new System.Windows.Forms.TextBox();
+            this.txtReader = new System.Windows.Forms.TextBox();
+            this.txtCardId = new System.Windows.Forms.TextBox();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnSend
+            // 
+            this.btnSend.Location = new System.Drawing.Point(41, 147);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(75, 23);
+            this.btnSend.TabIndex = 0;
+            this.btnSend.Text = "发送";
+            this.btnSend.UseVisualStyleBackColor = true;
+            this.btnSend.Click += new System.EventHandler(this.btnSend_Click);
+            // 
+            // txtController
+            // 
+            this.txtController.Location = new System.Drawing.Point(111, 12);
+            this.txtController.Name = "txtController";
+            this.txtController.Size = new System.Drawing.Size(148, 21);
+            this.txtController.TabIndex = 1;
+            // 
+            // txtReader
+            // 
+            this.txtReader.Location = new System.Drawing.Point(111, 55);
+            this.txtReader.Name = "txtReader";
+            this.txtReader.Size = new System.Drawing.Size(148, 21);
+            this.txtReader.TabIndex = 2;
+            // 
+            // txtCardId
+            // 
+            this.txtCardId.Location = new System.Drawing.Point(111, 98);
+            this.txtCardId.Name = "txtCardId";
+            this.txtCardId.Size = new System.Drawing.Size(148, 21);
+            this.txtCardId.TabIndex = 3;
+            // 
+            // btnClose
+            // 
+            this.btnClose.Location = new System.Drawing.Point(164, 147);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(75, 23);
+            this.btnClose.TabIndex = 4;
+            this.btnClose.Text = "关闭";
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(39, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 12);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "控制器";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(39, 58);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(41, 12);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "读卡器";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(39, 101);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(41, 12);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "读卡号";
+            // 
+            // FrmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(292, 182);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.txtCardId);
+            this.Controls.Add(this.txtReader);
+            this.Controls.Add(this.txtController);
+            this.Controls.Add(this.btnSend);
+            this.Name = "FrmMain";
+            this.Text = "车辆人员管理监控";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSend;
+        private System.Windows.Forms.TextBox txtController;
+        private System.Windows.Forms.TextBox txtReader;
+        private System.Windows.Forms.TextBox txtCardId;
+        private System.Windows.Forms.Button btnClose;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using vpms.vms;
+using WG3000_COMM.Core;
+using System.IO;
+
+namespace vpms
+{
+    public partial class FrmMain : Form
+    {
+        private wgWatchingService service = new wgWatchingService();
+        private RecordServcieService websvc = new RecordServcieService();
+        private FileStream fs = new FileStream("vms.log", FileMode.Create);
+        private StreamWriter sw;
+        System.Collections.Hashtable map = new System.Collections.Hashtable();//存放控制器读卡器映射
+
+        public FrmMain()
+        {
+            InitializeComponent();
+            this.OnStart();
+        }
+
+        protected void OnStart()
+        {
+            try
+            {
+                sw = new StreamWriter(this.fs);
+
+                Dictionary<int, wgMjController> controllers = new Dictionary<int, wgMjController>();
+                GetControllerListRequest r = new GetControllerListRequest();
+                Controller[] list = websvc.GetControllerList(r);
+                if (list != null)
+                {
+                    foreach (Controller s in list)
+                    {
+                        sw.WriteLine(s.sn + " " + s.ip);
+                        sw.Flush();
+
+                        wgMjController ctrl = new wgMjController();
+                        ctrl.ControllerSN = System.Int32.Parse(s.sn);
+                        ctrl.IP = s.ip;
+                        ctrl.PORT = 60000;
+                        controllers.Add(ctrl.ControllerSN, ctrl);
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("请求控制器列表返回null");
+                }
+
+                service.EventHandler += new OnEventHandler(evtNewInfoCallBack);
+                service.WatchingController = controllers;
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.Message);
+            }
+        }
+
+        private void evtNewInfoCallBack(string recd)
+        {
+            wgMjControllerSwipeRecord rec = new wgMjControllerSwipeRecord(recd);
+            onEvent(rec.ControllerSN, rec.CardID, rec.ReadDate, rec.ReaderNo);
+        }
+
+        private void onEvent(uint ControllerSN, uint CardID, DateTime ReadDate, byte ReaderNo) {
+            if (CardID > 1)
+            {
+                DateTime dt;
+
+                if (map.ContainsKey("" + CardID))
+                {
+                    dt = (DateTime)map["" + CardID];
+                }
+                else
+                {
+                    dt = DateTime.Now.AddDays(-1); //如果没有信息就相当于一天前刷过卡
+                }
+
+                // 如果上次读卡时间 距离本次读卡时间超过5秒 则应该记录本次刷卡
+                if (dt.AddSeconds(10) < ReadDate)
+                {
+                    try
+                    {
+                        RecordRequest req = new RecordRequest();
+                        req.cardNumber = "" + CardID;
+                        req.controllerSn = "" + ControllerSN;
+                        req.readerNumber = "" + ReaderNo;
+                        RecordResponse res = websvc.Record(req);
+
+                        sw.WriteLine("G: COMMD=" + res.command + " Reader=" + ReaderNo + " Date=" + ReadDate.ToString("yyyy-MM-dd HH:mm:ss") + " CardId=" + CardID +
+                            " ControllerSN=" + (int)ControllerSN + " Gan=" + res.gateNumber);
+
+                        if (res.command.ToLower().Equals("open"))
+                        {
+                            sw.Flush();
+                            service.WatchingController[(int)ControllerSN].RemoteOpenDoorIP(Int32.Parse(res.gateNumber));//抬杆
+                        }
+                    }
+                    catch (Exception e)
+                    {
+                        MessageBox.Show(e.Message);
+                    }
+                }
+
+                map["" + CardID] = ReadDate;
+            }
+            sw.WriteLine("");
+            sw.Flush();
+        }
+
+        protected void OnStop()
+        {
+            sw.Close();
+            fs.Close();
+        }
+
+        private void btnSend_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                onEvent(UInt32.Parse(txtController.Text), UInt32.Parse(txtCardId.Text),
+                    DateTime.Now, Byte.Parse(txtReader.Text));
+            }
+            catch (Exception ee)
+            {
+                MessageBox.Show(ee.Message);
+            }
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            this.Close();
+            System.Environment.Exit(0);
+        }
+    }
+}
